0d0f940e8550e15f5d148aadbf122825c6e886c2
Anastasia Slezko <slezkoanastasiia@gmail.com>
2022-09-19 10:26:38 +0300

    img/fix
    react/hw

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
-import homeBg from "../images/homeBg.jpg";
-import Container from "components/Container";
+// import homeBg from "../images/homeBg.jpg";
+// import Container from "components/Container";
 
-function HomePage() {
-  return <Container> {<img src={homeBg} alt="Phone book" />} </Container>;
-}
+// function HomePage() {
+//   return <Container> {<img src={homeBg} alt="Phone book" />} </Container>;
+// }
 
-export default HomePage;
+// export default HomePage;
